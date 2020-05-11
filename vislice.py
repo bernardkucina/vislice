b187a5c768542274b159d12a7633067cf89bc164
bernardkucina <kucina.berni@gmail.com>
2020-05-11 18:29:17 +0200

    popravek kodi

--- vislice.py
+++ vislice.py
@@ -6,7 +6,7 @@ NAPACNS_CRKA = '-'
 #konstante za zamgo
 ZMAGA = 'W'
 PORAZ = 'X'
-bazen = []
+bazen_besed = []
 with open('besede.txt', encoding='utf-8') as datoteka_bazena:
     for beseda in datoteka_bazena:
         bazen_besed.append(beseda.strip().lower())
@@ -14,7 +14,7 @@ with open('besede.txt', encoding='utf-8') as datoteka_bazena:
 class Igra:
     def __init__(self, geslo, crka=None):
         self.geslo = geslo
-        if crke is None:
+        if crka is None:
             self.crke = []
         else:
-            self.crke = crke
+            self.crke = crka
